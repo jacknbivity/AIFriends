d4d9d2f959103d943abb0f4f5b3b16a73ab7d5d3
xu hongjia <3182981394@qq.com>
2026-02-06 20:20:12 +0800

    数据库创建

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from web.models.user import UserProfile
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",)
