7cdc0f82a5026ca90c68b8aec8db53cc882ed54f
xu hongjia <3182981394@qq.com>
2026-02-10 15:30:40 +0800

    实现后端

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 from web.views.index import index
+from web.views.user.account.login import LoginAPIView
+from web.views.user.account.logout import LogoutView
+from web.views.user.account.refresh_token import RefreshTokenView
+from web.views.user.account.register import RegisterAPIView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/user/account/login/', LoginAPIView.as_view()),
+    path('api/user/account/logout/', LogoutView.as_view()),
+    path('api/user/account/register/', RegisterAPIView.as_view()),
+    path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('', index)
 ]
